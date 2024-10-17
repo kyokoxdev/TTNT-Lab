58140e13411d3970ef6f1f82bbcb0d8dcda631e2
Yokoyama Yuki <114841689+kyokoxdev@users.noreply.github.com>
2024-10-17 14:01:01 +0700

    init

--- source.py
+++ source.py
@@ -1,3 +1,15 @@
+import heapq
+
 class Graph:
     def graph_init(self):
         self.graph = {}
+        self.heuristic = {}
+    def add_edge(self, startpoint, endpoint, cost):
+        if startpoint not in self.graph:
+            self.graph[startpoint] = []
+        self.graph[startpoint].append((endpoint, cost))
+    def set_heuristic(self, node, value):
+        self.heuristic[node] = value
+    def bnb_algorithm(self, start, goal):
+        
+    
